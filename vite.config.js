5416ec91d5f883026c85e7eed289f9cf366cf3f2
Godseekers <ryan@rythebibleguy.com>
2026-02-12 18:28:35 +1300

    change to hard links for all files except jsons

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // If we are building for production, use the absolute URL. 
+    // Otherwise (dev mode), use the standard local path.
+    base: mode === 'production' 
+      ? 'https://react-spa-57t.pages.dev/' 
+      : '/',
+    server: {
+      host: '0.0.0.0',
+      port: 5173
+    }
   }
 })
